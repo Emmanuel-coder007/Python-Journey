6f4251e2bf00bc48904e6f0bdcb1ed85952c0bd3
Toluwani Shodeinde <shodeindetoluwani@gmail.com>
2026-03-12 19:28:12 +0000

    Using returns

---
+++ calculator.py
@@ -0,0 +1,27 @@
+# Write code below 💖
+
+def add(a, b):
+  value = a + b
+  return value
+
+def subtract(a, b):
+  value = a - b
+  return value
+
+def multiply(a, b):
+  value = a * b
+  return value
+
+def divide(a, b):
+  value = a / b
+  return value
+
+def exp(a, b):
+  value = a**b
+  return value
+
+print(add(3,4))
+print(subtract(5,4))
+print(multiply(3,3))
+print(divide(10,2))
+print(exp(2,3))
