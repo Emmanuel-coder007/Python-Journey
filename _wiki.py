da074869b396132cd8475d11becc9276f3dc2740
Toluwani Shodeinde <shodeindetoluwani@gmail.com>
2026-03-31 22:38:33 +0100

    Importing Libraries

---
+++ _wiki.py
@@ -0,0 +1,4 @@
+import wikipedia
+
+print(wikipedia.summary("Wikipedia"))
+# Wikipedia (/ˌwɪkɨˈpiːdiə/ or /ˌwɪkiˈpiːdiə/ WIK-i-PEE-dee-ə) is a collaboratively edited, multilingual, free Internet encyclopedia supported by the non-profit Wikimedia Foundation...
