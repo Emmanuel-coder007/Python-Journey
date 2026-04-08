ab38807d8b8194fd7694d37b514e6de572cb880a
Toluwani Shodeinde <shodeindetoluwani@gmail.com>
2026-04-08 16:54:13 +0100

    Learnt About tuples

---
+++ Intermidiate Python/Tuples.py
@@ -0,0 +1,19 @@
+person = ('Kat', 24)
+navy_blue = (0, 0, 128)
+
+# valid tuple
+t1 = ('hi',)
+
+# valid tuple
+t2 = 'hi',
+
+# not a tuple
+t3 = ('hi')
+
+
+# Combining tuples
+t1 = 'a',
+t2 = 'b',
+t3 = t1 + t2
+
+print(t3)  # Output: ('a', 'b')
---
+++ Intermidiate Python/cities.py
@@ -0,0 +1,13 @@
+# Write code below 💖
+city = 'London',
+dayo = '45.433', '67.445',
+semzi = '87.445', '99.345',
+ebuka = '33.657', '66.436',
+
+print(dayo)
+print(semzi)
+print(ebuka)
+
+total = dayo + semzi + ebuka
+
+print(total)
