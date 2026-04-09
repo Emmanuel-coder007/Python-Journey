6a9721bd208fcc1a4881a9866ae2e17330fc5029
Toluwani Shodeinde <shodeindetoluwani@gmail.com>
2026-04-09 13:39:37 +0100

    Learnt about Sets

---
+++ Intermidiate Python/fruits_list.py
@@ -0,0 +1,7 @@
+# Write code below 💖
+fruits = {'apple','banana', 'pinapple'}
+friendFruits = {'pear', 'orange','apple'}
+
+print(fruits.union(friendFruits))
+print(fruits.intersection(friendFruits))
+print(fruits.difference(friendFruits))
