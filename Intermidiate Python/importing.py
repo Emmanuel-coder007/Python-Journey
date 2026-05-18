89dd6ecddaa36600c38d38411e2126d80469bd82
Toluwani Shodeinde <shodeindetoluwani@gmail.com>
2026-05-18 16:26:44 +0100

    importing

---
+++ Intermidiate Python/importing.py
@@ -0,0 +1,18 @@
+#this is a module that we can import into other files
+import math    #from maths import sqrt
+
+math.sqrt(16)
+
+#orrrr
+
+from math import sqrt, pi    #importing specific items from a module
+# this imported square root and pi from the math module, so we can use them directly without the math. prefix
+sqrt(16)
+pi
+
+#other useful modules include:
+# datetime - for working with dates and times
+# random - for generating random numbers
+#os - for interacting with the operating system
+#json - for working with JSON data
+#pandas - for data manipulation and analysis
